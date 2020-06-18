730bd5ba83efafbc5368d5fcc0fa266bcbb2f056
MinexAutomation <minex.automation@gmail.com>
2020-06-17 17:51:38 -0700

    Added R5T.D0016.Base (IValueEqualityComparer<T>) service definiton library. Added R5T.D0010.Base (IMessageSink) and R5T.T0001 (Message types) project-reference dependencies. Added IValueEqualityComparer<T> service definition interface type.

---
+++ source/R5T.D0016.Base/Code/Services/Definitions/IValueEqualityComparer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.D0010;
+
+
+namespace R5T.D0016
+{
+    public interface IValueEqualityComparer<T>
+    {
+        Task<bool> Equals(T x, T y, IMessageSink messageSink);
+    }
+}
