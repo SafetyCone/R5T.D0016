7f1ecd55c6c12cce1fe2aa873e520345c5e4c46e
davidcoats <david@getsafetycone.com>
2022-04-27 15:26:45 -0700

    Interim changes.

--- source/R5T.D0016.Base/Code/Services/Definitions/IValueEqualityComparer.cs
+++ source/R5T.D0016.Base/Code/Services/Definitions/IValueEqualityComparer.cs
@@ -2,11 +2,13 @@
 using System.Threading.Tasks;
 
 using R5T.D0010;
+using R5T.T0064;
 
 
 namespace R5T.D0016
 {
-    public interface IValueEqualityComparer<T>
+    [ServiceDefinitionMarker]
+    public interface IValueEqualityComparer<T> : IServiceDefinition
     {
         Task<bool> Equals(T x, T y, IMessageSink messageSink);
     }
